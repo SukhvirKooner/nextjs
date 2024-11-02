366cc247c906cc9881cb5ae417f3804ae872aea6
SukhvirKooner <sukhvirkooner05@gmail.com>
2024-11-02 12:30:42 +0530

    first commit

---
+++ my-app/app/(auth)/layout.tsx
@@ -0,0 +1,17 @@
+
+export default function({children}:{
+    children:React.ReactNode
+}){
+    return(
+        <div>
+
+        
+    <div className="border text-center">
+        <h1>50% offf</h1>
+        
+    </div>
+    {children}
+    </div>
+    
+    )
+}
